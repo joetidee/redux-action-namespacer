29e0dcd93f74d9c183753ceead07139f9712f511
joetidee <joe@saeker.co.uk>
2017-04-06 22:50:45 +0100

    Converted required schema to nested array structure

--- src/index.js
+++ src/index.js
@@ -1,38 +1,47 @@
 'use strict';
 
 /**
- * Converts a hash of structured action types into .
- * @param {object} hash - A structured hash of action types.
- * @param {string} 
- * @return {object} messages
+ * Converts an array of structured action types into an object.
+ * @param {array} arr - A structured hash of action types.
+ * @param {string} prependName - A value that represents the current namespace name. It is used when the function calls itself.
+ * @return {object}
  *
- * Example: 
- * export const ACTIONS = namespaceActionTypes({
- *   dataTable: {
- *       loading: true,
- *       controls: {
- *           load: true,
- *           save: true
- *       }
- *   }
- * });
+ * Example of usage:
+ * export const ACTIONS = nsActionTypes([
+ *  'table', [
+ *      'loading',
+ *      'data',
+ *      'query', [
+ *          'sort',
+ *          'limit',
+ *          'search'
+ *      ]
+ *  ]
+ *]);
  */
- const namespaceActionTypes = (hash, prependName=null) => {
+const nsActionTypes = (arr, prependName=null) => {
 
     let actionTypeObj = {};
-    for(let key in hash){
+    let newArr = arr.slice();
+    for(let i=0 ; i < newArr.length ; i++){
 
-        let value = hash[key];
+        let value = newArr[i];
+        let next = newArr[(i+1)];
+
         let namespacedName;
         if (prependName !== null) {
-            namespacedName = prependName + '.' + key;
-        } else {
-            namespacedName = key;
+            namespacedName = prependName + '.' + value;
         }
-        if (value === true) {
-            actionTypeObj[key] = namespacedName;
+        else{
+            namespacedName = value;
+        }
+
+        if (Array.isArray(next)) {
+            actionTypeObj[value] = nsActionTypes(next, namespacedName);
         } else {
-            actionTypeObj[key] = namespaceActionTypes(value, namespacedName);
+            if (!Array.isArray(value)) {
+                actionTypeObj[value] = namespacedName;
+            }
         }
     }
 
@@ -40,7 +49,44 @@
 };
 
 
-var reduxActionNamespacer = {
-    nsActionTypes: nsActionTypes
+/**
+ * Tests an array to see if there are any duplicate keys.
+ * @param {array} arr - A structured array of action types.
+ * @param {number} level - The depth level of the array. It is used when the function calls itself.
+ * @return {true} - Throws an Error if array is not supplied/is invalid.
+ */
+const nsActionTypesCheck = (arr, level=0) => {
+
+    if(typeof arr === "undefined"){
+        throw new Error("Action type array was not supplied to the function nsActionTypesCheck()");
+    }
+
+    let keysArray = [];
+    let newArr = arr.slice();
+    for(let i=0 ; i < newArr.length ; i++){
+
+        let value = newArr[i];
+        let next = newArr[(i+1)];
+
+        if(keysArray.indexOf(value) !== -1){
+            throw new Error("Duplicate action type value '" + value + "' at level " + level + ".");
+        }
+        else{
+            keysArray.push(value);
+        }
+
+        if (Array.isArray(next)) {
+            level++;
+            nsActionTypesCheck(next, level);
+        }
+    }
+
+    return true;
+};
+
+
+const reduxActionNamespacer = {
+    nsActionTypes: nsActionTypes,
+    nsActionTypesCheck: nsActionTypesCheck
 };
 module.exports = reduxActionNamespacer;
--- test/test.js
+++ test/test.js
@@ -2,16 +2,71 @@ require('babel-register')();
 import chai, {expect, assert} from 'chai';
 let chaiSubset = require('chai-subset');
 chai.use(chaiSubset);
-import { nsActionTypes } from '../src/index.js';
+import { nsActionTypes, nsActionTypesCheck } from '../src/index.js';
+
 
 describe('reduxActionNamespacer', function() {
 
+    let expectedValidResult = {
+        table: {
+            loading: 'table.loading',
+            data: 'table.data',
+            query: {
+                sort: 'table.query.sort',
+                limit: 'table.query.limit',
+                search: 'table.query.search',
+            }
+        }
+    };
+
+    let actionsTypeHashValid = [
+        'table', [
+            'loading',
+            'data',
+            'query', [
+                'sort',
+                'limit',
+                'search'
+            ]
+        ]
+    ];
+
+    let actionsTypeHashInvalid = [
+        'table', [
+            'loading',
+            'data',
+            'query', [
+                'sort',
+                'limit',
+                'search'
+            ]
+        ],
+        'table', [
+            'data'
+        ]
+    ];
+
     describe('nsActionTypes()', function() {
-        it('should set the locale', function () {
-            let localeSet = 'en-GB';
-            setLocale(localeSet);
-            let localeGet = getLocale();
-            expect(localeSet).to.equal(localeGet);
+        it('should return an object', function () {
+            let result = nsActionTypes(actionsTypeHashValid);
+            expect(result).to.be.an('object');
+        });
+        it('should return an object in the correct format', function () {
+            let result = nsActionTypes(actionsTypeHashValid);
+            expect(result).to.deep.equal(expectedValidResult);
+        });
+    });
+
+    describe('nsActionTypesCheck()', function() {
+        it('should return true on a valid array', function () {
+            let result = nsActionTypesCheck(actionsTypeHashValid);
+            expect(result).to.be.true;
+        });
+        it('should throw an error if a array is not supplied', function () {
+            expect(() => nsActionTypesCheck()).to.throw(Error);
+        });
+        it('should throw an error if a array is invalid', function () {
+            expect(() => nsActionTypesCheck(actionsTypeHashInvalid)).to.throw(Error);
         });
     });
 });
